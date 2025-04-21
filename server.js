5edaefdfa2475fdb1cb022bb93b6d29dd0e24446
Joe19071991 <lauranove66@gmail.com>
2025-04-21 19:47:14 +0200

    installation de body-parser

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 let express = require("express");
 let app = express();
+const bodyParser = require("body-parser");
 
+// Moteur de template
 app.set("view engine", "ejs");
 
+//Middleware
 app.use("/assets", express.static("public"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
+//Routes
 app.get("/", (req, res) => {
   res.render("pages/index", { test: "Hello World" });
+});
+
+app.post("/", (req, res) => {
+  console.log(req.body);
 });
 
 app.listen(8885);
